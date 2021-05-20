99bc853900351285422fedd3fecc6fe1a66e7347
mgrddsj <Jesse_Xu@live.com>
2021-05-20 11:44:10 +0800

    Updated week5 to showcase the code.

--- Week5.py
+++ Week5.py
@@ -23,18 +23,19 @@ def main():
     objp = np.zeros((1, CHESSBOARD[0]*CHESSBOARD[1], 3), np.float32)
     objp[0,:,:2] = np.mgrid[0:CHESSBOARD[0], 0:CHESSBOARD[1]].T.reshape(-1, 2)
 
-    ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-    if ret == True:
-        objpoints.append(objp)
-        # refining pixel coordinates for given 2d points.
-        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+    with st.echo():
+        ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+        if ret == True:
+            objpoints.append(objp)
+            # refining pixel coordinates for given 2d points.
+            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
 
-        imgpoints.append(corners2)
+            imgpoints.append(corners2)
 
-        # Draw and display the corners
-        corners_img = cv2.drawChessboardCorners(image, CHESSBOARD, corners2,ret)
-        st.image(corners_img, use_column_width=True, caption="Chessboard detection results 棋盘检测结果", channels="BGR")
-        st.info("Please zoom in to see the chessboard corners found 请放大图片来看清棋盘标定结果")
+            # Draw and display the corners
+            corners_img = cv2.drawChessboardCorners(image, CHESSBOARD, corners2,ret)
+            st.image(corners_img, use_column_width=True, caption="Chessboard detection results 棋盘检测结果", channels="BGR")
+            st.info("Please zoom in to see the chessboard corners found 请放大图片来看清棋盘标定结果")
 
 if __name__ == "__main__":
     main()
