5102b887a6f2881132295820a10fb394c3c5aec9
mgrddsj <Jesse_Xu@live.com>
2021-05-20 11:45:43 +0800

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -5,25 +5,25 @@ import glob
 from PIL import Image
 
 def main():
-    file_path = "img/chessboard.webp"
-
-    image = cv2.imread(file_path)  
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    st.image(image, use_column_width=True, caption="Original Image 原图", channels="BGR")
-
-    CHESSBOARD = (9, 7)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # Creating vector to store vectors of 3D points for each CHESSBOARD image
-    objpoints = []
-    # Creating vector to store vectors of 2D points for each CHESSBOARD image
-    imgpoints = [] 
-
-    # Defining the world coordinates for 3D points
-    objp = np.zeros((1, CHESSBOARD[0]*CHESSBOARD[1], 3), np.float32)
-    objp[0,:,:2] = np.mgrid[0:CHESSBOARD[0], 0:CHESSBOARD[1]].T.reshape(-1, 2)
-
     with st.echo():
+        file_path = "img/chessboard.webp"
+
+        image = cv2.imread(file_path)  
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        st.image(image, use_column_width=True, caption="Original Image 原图", channels="BGR")
+
+        CHESSBOARD = (9, 7)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        # Creating vector to store vectors of 3D points for each CHESSBOARD image
+        objpoints = []
+        # Creating vector to store vectors of 2D points for each CHESSBOARD image
+        imgpoints = [] 
+
+        # Defining the world coordinates for 3D points
+        objp = np.zeros((1, CHESSBOARD[0]*CHESSBOARD[1], 3), np.float32)
+        objp[0,:,:2] = np.mgrid[0:CHESSBOARD[0], 0:CHESSBOARD[1]].T.reshape(-1, 2)
+        
         ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
         if ret == True:
             objpoints.append(objp)
